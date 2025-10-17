39c72d6f283f8e2a7d10ddeb78941a1e571a7460
TokenBro11400 <Ntmv3009@gmail.com>
2025-10-17 23:35:48 +1100

    NSOS-Installer Added

--- NSOS-Hub.lua
+++ NSOS-Hub.lua
@@ -1,21 +1,19 @@
-print(" _____ _____ _____ _____ ")
-print("│   │ │   __│     │   __│")
-print("│ │ │ │__   │  │  │__   │")
-print("│_│___│_____│_____│_____│")
-print("")
-print("--------------------------------------------------------------------------------")
-print("Welcome to NSOS-Hub V-ALPHA1")
+
+print("------------------------------")
+print("Welcome to NSOS-Hub V-ALPHA2")
 term.setTextColor(colors.yellow)
 print("WARNING: This is an alpha version, expect bugs and incomplete features.")
-tern.setTextColor(colors.white)
-print("--------------------------------------------------------------------------------")
+term.setTextColor(colors.white)
+print("------------------------------")
 
 local DataSave = "NSOS-DataSave"
 
 sleep(.5)
 
 if not fs.exists(DataSave) then
+    term.setTextColor(colors.red)
     print("First time setup / DataSave corruption detected. Setting up DataSave...")
+    term.setTextColor(colors.white)
     fs.open(DataSave, "w").close()
 end
 
