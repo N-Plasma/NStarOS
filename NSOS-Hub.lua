e524c0788c8f36bc2b0cd264db146b85a5c674b4
TokenBro11400 <Ntmv3009@gmail.com>
2025-10-24 16:23:52 +1100

    Updater & NSOS-Hub Setup Complete, V-Beta-1

--- NSOS-Hub.lua
+++ NSOS-Hub.lua
@@ -1,12 +1,15 @@
+shell.run("clear")
+
+-- Setup
 
 print("------------------------------")
-print("Welcome to NSOS-Hub V-ALPHA2")
+print("Welcome to NSOS-Hub V-Beta-1.0")
 term.setTextColor(colors.yellow)
 print("WARNING: This is an alpha version, expect bugs and incomplete features.")
 term.setTextColor(colors.white)
 print("------------------------------")
 
-local DataSave = "NSOS-DataSave"
+local DataSave = "NSOS-DataSave.txt"
 
 sleep(.5)
 
@@ -17,5 +20,32 @@ if not fs.exists(DataSave) then
     fs.open(DataSave, "w").close()
 end
 
+-- Update Checker
 
+local function checkForUpdates()
+    local success, response = pcall(function()
+        return http.get("https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Version.txt")
+end)
 
+    if success and response then
+        local latestVersion = response.readAll()
+        response.close()
+
+        local currentVersionFile = fs.open("NSOS-Version.txt", "r")
+        local currentVersion = currentVersionFile.readAll()
+        currentVersionFile.close()
+
+        if latestVersion ~= currentVersion then
+            term.setTextColor(colors.yellow)
+            print("Update available, would you like to update now? (y/n)")
+            term.setTextColor(colors.white)
+            local updatewant = read()
+                if updatewant == "Y" or updatewant == "y" then
+                shell.run("NSOS-Installer-Updater.lua")
+                fs.open("NSOS-Version.txt", "w").write(UPDATE)
+                else
+                print("Continuing without update...")
+            end
+        end
+    end
+end
---
+++ NSOS-Installer-Updater.lua
@@ -0,0 +1,66 @@
+-- NSOS Updater
+
+local function UpdateNSOS()
+
+            if fs.exists("NSOS-IveGotGames.lua") then
+                fs.delete("NSOS-IveGotGames.lua")
+                shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-IveGotGames.lua")
+        end
+            if fs.exists("NSOS-REDNET.lua") then
+                fs.delete("NSOS-REDNET.lua")
+                    shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-REDNET.lua")
+        end
+            if fs.exists("NSOS-Printer.lua") then
+                fs.delete("NSOS-Printer.lua")
+                shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Printer.lua")
+        end
+    
+
+    fs.delete("NSOS-Hub.lua")
+    fs.delete("NSOS-Version.txt")
+    shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Hub.lua")
+    shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Version.txt")
+    print("NSOS Updated, Restarting NSOS-Hub...")
+    shell.run("NSOS-Hub.lua")
+end
+
+local UpdateStatus = fs.open("NSOS-Version.txt", "r")
+local Check = UpdateStatus.readAll()
+fs.close(UpdateStatus)
+
+if Check == "UPDATE" then
+    print("Update Code Detected, Re-Installing NSOS (if data is affected there will be a warning when starting NSOS Again with a option to re-format so minimal data is lost)")
+    UpdateNSOS()
+end
+-- NSOS Installer
+
+print("This will install NSOS in the current directory.")
+print("Proceed? (y/n)")
+local input = read()
+if input == "Y" or input == "y" then
+    print("Choose Install options (NSOS-Hub will be installed with all options, type in any order any options multiple options allowed type None to only get NSOS-Hub):")
+    print("")
+    print("1. NSOS-REDNET (Requires modem)")
+    print("")
+    print("2. NSOS-Printer (Requires printer)")
+    print("")
+    print("3. NSOS-IveGotGames (Requires Speaker & Monitor)")
+    print("")
+    local choice = read()
+        if string.find(choice, "1") then
+            shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-REDNET.lua")
+        end
+        if string.find(choice, "2") then
+            shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Printer.lua")
+        end
+        if string.find(choice, "3") then
+            shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/IveGotGames.lua")
+        end
+    shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main//NSOS-Hub.lua")
+    shell.run("wget https://raw.githubusercontent.com/N-Plasma/NStarOS/main/NSOS-Version.txt")
+    print("NSOS installed.")
+else
+    print("Installation cancelled.")
+end
+
+shell.run("NSOS-Hub.lua")
